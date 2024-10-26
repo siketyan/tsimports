b3d8d858b3814b7b45859edbeaea826974f86dc2
Naoki Ikeguchi <me@s6n.jp>
2024-10-26 20:53:07 +0900

    feat: Support globs as input

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use biome_console::{markup, ColorMode, Console, EnvConsole, LogLevel};
 use biome_diagnostics::PrintDiagnostic;
 use biome_js_syntax::JsFileSource;
 use clap::{Parser, ValueEnum};
+use glob::glob;
 use tsimports::{tsimports, Error};
 
 #[derive(Copy, Clone, Debug, Default, ValueEnum)]
@@ -21,8 +22,8 @@ enum Language {
 #[derive(Debug, Parser)]
 #[command(about, version)]
 struct Args {
-    /// Path of the file to organize imports. Defaults to the standard input.
-    path: Option<PathBuf>,
+    /// Paths or globs of files to organize imports. Defaults to the standard input.
+    paths: Option<Vec<String>>,
 
     /// Specify the language to parse the input as. Inferred by the file extension by default.
     #[clap(short, long)]
@@ -46,9 +47,28 @@ fn main() {
 fn run(console: &mut impl Console) -> Result<()> {
     let args = Args::parse();
 
+    if let Some(paths) = &args.paths {
+        for path in paths.iter() {
+            for entry in glob(path)? {
+                run_single(Input::File(entry?), &args, console)?;
+            }
+        }
+    } else {
+        run_single(Input::Stdin, &args, console)?;
+    }
+
+    Ok(())
+}
+
+enum Input {
+    File(PathBuf),
+    Stdin,
+}
+
+fn run_single(input: Input, args: &Args, console: &mut impl Console) -> Result<()> {
     let mut source = JsFileSource::js_module();
-    let input = match &args.path {
-        Some(path) => {
+    let buf = match &input {
+        Input::File(path) => {
             if let Some(ext) = path.extension().and_then(|ext| ext.to_str()) {
                 match ext {
                     "jsx" => source = JsFileSource::jsx(),
@@ -60,7 +80,7 @@ fn run(console: &mut impl Console) -> Result<()> {
 
             read_to_string(path)?
         }
-        None => {
+        _ => {
             let mut buf = String::new();
             stdin().read_to_string(&mut buf)?;
             buf
@@ -78,15 +98,15 @@ fn run(console: &mut impl Console) -> Result<()> {
         }
     } else {
         console.println(LogLevel::Error, markup! {
-            <Warn>
-                "\u{26a0} Input language is not specified, assuming as an ECMAScript module. Use "
-                <Emphasis>"--language <js|jsx|ts|tsx>"</Emphasis>
-                " option to override."
-            </Warn>
-        });
+        <Warn>
+            "\u{26a0} Input language is not specified, assuming as an ECMAScript module. Use "
+            <Emphasis>"--language <js|jsx|ts|tsx>"</Emphasis>
+            " option to override."
+        </Warn>
+    });
     };
 
-    let output = match tsimports(input.as_str(), source) {
+    let output = match tsimports(buf.as_str(), source) {
         Ok(o) => o,
         Err(e) => match e {
             Error::Parser(diags) => {
@@ -104,7 +124,7 @@ fn run(console: &mut impl Console) -> Result<()> {
     };
 
     if args.write {
-        let Some(path) = args.path else {
+        let Input::File(path) = input else {
             return Err(anyhow!(
                 "Can't write the result as the input was from the standard input."
             ));
